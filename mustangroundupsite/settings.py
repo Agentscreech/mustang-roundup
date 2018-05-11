5f7e7d623beba1b10102bd9bfeb6ae9695649a54
Travis Walentin <travis.walentin@gmail.com>
2018-05-10 17:34:39 -0700

    fixing allowed hosts

--- mustangroundupsite/settings.py
+++ mustangroundupsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["mustangroundup.herokuapp.com:","127.0.0.1:8000", "localhost:8000", "localhost"]
+ALLOWED_HOSTS = [".herokuapp.com","127.0.0.1:8000", "localhost:8000", "localhost"]
 
 
 # Application definition
